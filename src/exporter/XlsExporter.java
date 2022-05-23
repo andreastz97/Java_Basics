ce957e3df7b909f17bd46bc6d14c743d70d97824
Andreas Tzioukalias <andreasogame@gmail.com>
2022-05-23 18:35:28 +0300

    Added my project

--- src/exporter/XlsExporter.java
+++ src/exporter/XlsExporter.java
@@ -14,11 +14,15 @@ import java.util.List;
 public class XlsExporter implements Exporter {
 
     public void export(List<Employee> employees) {
+        List<Manager> managers = new ArrayList<>();
+
         if (!employees.isEmpty() && employees.get(0) instanceof Manager) {
+            for (int i = 0; i < employees.size(); i++) {
+                managers.add((Manager) employees.get(i));
+            }
             try {
                 Workbook workbook = new XSSFWorkbook();
                 Sheet sheet = workbook.createSheet("Employees");
-
 
                 String[] columnHeadings = {"ID", "First Name", "Last Name", "Number Of Sellers", "Country"};
                 Font headerFont = workbook.createFont();
@@ -38,9 +42,11 @@ public class XlsExporter implements Exporter {
                 for (int i = 0; i < employees.size(); i++) {
 
                     Row row1 = sheet.createRow(++rownum);
-                    row1.createCell(0).setCellValue(employees.get(i).getId());
-                    row1.createCell(1).setCellValue(employees.get(i).getFirstName());
-                    row1.createCell(2).setCellValue(employees.get(i).getLastName());
+                    row1.createCell(0).setCellValue(managers.get(i).getId());
+                    row1.createCell(1).setCellValue(managers.get(i).getFirstName());
+                    row1.createCell(2).setCellValue(managers.get(i).getLastName());
+                    row1.createCell(3).setCellValue(managers.get(i).getNumberOfSellers());
+                    row1.createCell(4).setCellValue(managers.get(i).getCountry());
                 }
                 FileOutputStream fileOutput = new FileOutputStream("NewFile2.xls");
                 workbook.write(fileOutput);
@@ -50,12 +56,15 @@ public class XlsExporter implements Exporter {
             }
         }
         if (!employees.isEmpty() && employees.get(0) instanceof Seller) {
+            List<Seller> sellers = new ArrayList<>();
+            for (int i = 0; i < employees.size(); i++) {
+                sellers.add((Seller) employees.get(i));
+            }
             try {
                 Workbook workbook = new XSSFWorkbook();
                 Sheet sheet = workbook.createSheet("Employees");
 
-
-                String[] columnHeadings = {"ID", "First Name", "Last Name", "Country", "Average Sales", "Active"};
+                String[] columnHeadings = {"ID", "First Name", "Last Name", "City", "Average Sales", "Active"};
                 Font headerFont = workbook.createFont();
                 headerFont.setBoldweight((short) 12);
                 headerFont.setFontHeightInPoints((short) 12);
@@ -73,9 +82,13 @@ public class XlsExporter implements Exporter {
                 for (int i = 0; i < employees.size(); i++) {
 
                     Row row1 = sheet.createRow(++rownum);
-                    row1.createCell(0).setCellValue(employees.get(i).getId());
-                    row1.createCell(1).setCellValue(employees.get(i).getFirstName());
-                    row1.createCell(2).setCellValue(employees.get(i).getLastName());
+                    row1.createCell(0).setCellValue(sellers.get(i).getId());
+                    row1.createCell(1).setCellValue(sellers.get(i).getFirstName());
+                    row1.createCell(2).setCellValue(sellers.get(i).getLastName());
+                    row1.createCell(3).setCellValue(sellers.get(i).getCity());
+                    row1.createCell(4).setCellValue(sellers.get(i).getAverageSales());
+                    row1.createCell(5).setCellValue(sellers.get(i).isActive());
+
                 }
                 FileOutputStream fileOutput = new FileOutputStream("NewFile2.xls");
                 workbook.write(fileOutput);
@@ -83,88 +96,6 @@ public class XlsExporter implements Exporter {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
-
         }
-
-
     }
 }
-
-
-           /* Row row = sheet.createRow(1);
-            Cell cell0 = row.createCell(1);
-
-
-            Cell cell1 = row.createCell(1);
-            Cell cell2 = row.createCell(1);
-
-
-
-            for (int i = 0; i < employees.size(); i++) {
-                cell0.setCellValue("ID");
-                cell1.setCellValue("First Name");
-                cell2.setCellValue("Last Name");
-
-                Row row1 = sheet.createRow(rownum++);
-                row1.createCell(0).setCellValue(employees.get(i).getId());
-                row1.createCell(1).setCellValue(employees.get(i).getFirstName());
-                row1.createCell(2).setCellValue(employees.get(i).getLastName());
-
-            }
-
-            FileOutputStream fileOutput = new FileOutputStream("NewFile2.xls");
-            workbook.write(fileOutput);
-            fileOutput.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-} */
-
-          /*  for (int i = 0; i < employees.size(); i++) {
-                for (int j = 0; j < 3; j++) {
-                    Cell cell = row.createCell(j);
-                    for (int k = 0; k < employees.size(); k++) {
-
-                        if (cell.getColumnIndex() == 0) {
-                            cell.setCellValue(employees.get(j).getId());
-                        } else if (cell.getColumnIndex() == 1) {
-                            cell.setCellValue(employees.get(j).getFirstName());
-                        } else cell.setCellValue(employees.get(k).getLastName());
-
-                    }
-                    Cell cell5 = row.createCell(j + 1);
-                }
-                row = sheet.createRow(i + 1);
-            }
-
-            FileOutputStream out = new FileOutputStream(new File("NewFile.xls"));
-            workbook.write(out);
-            out.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (IndexOutOfBoundsException e) {
-            throw new RuntimeException(e);
-        } finally {
-
-        }
-    }
-} */
-
-          /*     PLANB    sheet.getCells().importCustomObjects((Collection)employees,
-                    new String[] { "ID", "First Name","Last Name" }, // propertyNames
-                    true, // isPropertyNameShown
-                    0, // firstRow
-                    0, // firstColumn
-                    employees.size(), // Number of objects to be exported
-                    true, // insertRows
-                    null, // dateFormatString
-                    false); // convertStringToNumber
-            //XSSFWorkbook workbook = new XSSFWorkbook();
-            XSSFSheet sheet = workbook.createSheet("sheet1");
-            Workbook workbook = new Workbook();
-}*/
