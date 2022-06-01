5f99b89044bb9172dd46281d2b2a4e635dd84293
Andreas Tzioukalias <andreasogame@gmail.com>
2022-06-01 20:28:56 +0300

    magic string remove 3

--- src/exporter/XlsExporter.java
+++ src/exporter/XlsExporter.java
@@ -12,31 +12,32 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class XlsExporter implements Exporter {
-    private static final String[] columnHeadingsManagers = {"ID", "First Name", "Last Name", "Number Of Sellers", "Country"};
-    private static final String[] columnHeadingsSellers = {"ID", "First Name", "Last Name", "City", "Average Sales", "Active"};
-    Workbook workbook = new XSSFWorkbook();
-    Sheet sheet = workbook.createSheet("Employees");
-    OutputDesigner design = new OutputDesigner();
-    HeadFontDesigner headerStyle = new HeadFontDesigner();
+    private static final String[] COLUMN_HEADINGS_MANAGERS = {"ID", "First Name", "Last Name", "Number Of Sellers", "Country"};
+    private static final String[] COLUMN_HEADINGS_SELLERS = {"ID", "First Name", "Last Name", "City", "Average Sales", "Active"};
+    private static final String FILEPATH = "NewFile2.xls";
 
     public void export(List<Employee> employees) throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Employees");
+        OutputDesigner design = new OutputDesigner();
+        HeadFontDesigner headerStyle = new HeadFontDesigner();
+
         if (!employees.isEmpty() && employees.get(0) instanceof Manager) {
-            try (FileOutputStream fileOutput = new FileOutputStream("NewFile2.xls");) {
+            try (FileOutputStream fileOutput = new FileOutputStream(FILEPATH);) {
                 design.design(sheet);
-                headerStyle.headDesign(columnHeadingsManagers, sheet);
+                headerStyle.headDesign(COLUMN_HEADINGS_MANAGERS, sheet);
                 CellFillerManagers cellFillerManagers = new CellFillerManagers();
                 cellFillerManagers.fillCells(employees, sheet);
                 workbook.write(fileOutput);
             }
         }
         if (!employees.isEmpty() && employees.get(0) instanceof Seller) {
-            try (FileOutputStream fileOutput = new FileOutputStream("NewFile2.xls");) {
+            try (FileOutputStream fileOutput = new FileOutputStream(FILEPATH);) {
                 design.design(sheet);
-                headerStyle.headDesign(columnHeadingsSellers, sheet);
+                headerStyle.headDesign(COLUMN_HEADINGS_SELLERS, sheet);
                 CellFillerSellers cellFillerSellers = new CellFillerSellers();
                 cellFillerSellers.fillCells(employees, sheet);
                 workbook.write(fileOutput);
